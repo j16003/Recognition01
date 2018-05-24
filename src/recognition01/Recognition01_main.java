6144fe9ac07830cb75cc777ffcad079b9d4de930
j16003 <j16003@j16003-PC>
2018-05-24 12:31:11 +0900

    yes

--- src/recognition01/Recognition01_main.java
+++ src/recognition01/Recognition01_main.java
@@ -2,17 +2,21 @@ package recognition01;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
+
 
 public class Recognition01_main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-		
+
+
 		VisualRecognition service = new VisualRecognition("2018-03-19");
 		service.setApiKey("j16003");
 
@@ -27,8 +31,26 @@ public class Recognition01_main {
 		}
 		DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
 		System.out.println(result);
+		ObjectMapper mapper = new ObjectMapper();
+		String s = String.valueOf(result);
+		JsonNode node = null;
+		try {
+			node = mapper.readTree(s);
+			int age_min = node.get("images").get(0).get("faces").get(0).get("age").get("min").asInt();
+			System.out.println("age_min : " + age_min);
+			int age_max = node.get("images").get(0).get("faces").get(0).get("age").get("max").asInt();
+			System.out.println("age_max : " + age_max);
+			double age_score = node.get("images").get(0).get("faces").get(0).get("age").get("score").asDouble();
+			System.out.println("age_score : " + age_score);
+			String gender = node.get("images").get(0).get("faces").get(0).get("gender").get("gender").toString();
+			System.out.println("Gender : " + gender);
+			double gender_score = node.get("images").get(0).get("faces").get(0).get("gender").get("score").asDouble();
+			System.out.println("Gender_score : " + gender_score);
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 
-		
 	}
 
 }
